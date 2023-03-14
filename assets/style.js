e59e75a0420bae61c1bd90e12f8044f6291cd35f
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-03-14 15:45:27 -0700

    added my questions in as comments

--- assets/style.js
+++ assets/style.js
@@ -12,9 +12,21 @@
 // THEN I can save my initials and score
 
 // Assignment Code
-var 
+var generateScore 
 var questions
 var answers 
 var userChoice = ''; 
 
 
+// We need 5 questions with 4 choices 
+    // we also need to let the user know if the choice they picked is incorrect or correct
+
+    // Question 1: Commonly used data types DO Not include: 1. Strings 2. booleans 3. alerts 4. numbers 
+
+    // Question 2: The condition in an if / else statement is enclosed with _____. 1. quotes 2. curly brackets 3. parenthesis 4. square brackets 
+
+    // Question 3: Arrays in JavaScript can be used to store ___. 1. numbers and strings 2. other arrays 3. booleans. 4. all of the above. 
+
+    // Question 4: String values must be enclosed with ___ when being assigned to variables. 1. commas 2. curly brackets 3. quotes 4. parenthesis 
+
+    // Question 5: A very useful tool used during development and debugging for printing content to the debugger is: 1. JavaScript 2. terminal/bash 3. for loops 4. console.log
