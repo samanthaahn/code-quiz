58e830fe5cac4825bc76303c4b28c8071df03bd4
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-03-14 21:52:19 -0700

    got it so the first page is working and when you click start it starts the quiz

--- assets/style.js
+++ assets/style.js
@@ -1,16 +1,3 @@
-// Acceptance Criteria
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-
 // Assignment Code
 
 var quizQuestions = document.querySelector(".quiz-questions");
@@ -19,9 +6,14 @@ var choiceA = document.querySelector("#a");
 var choiceB = document.querySelector("#b");
 var choiceC = document.querySelector("#c");
 var choiceD = document.querySelector("#d");
-var timerEl = document.querySelector('#countdown');
+var timerEl = document.querySelector("#countdown");
+var buttonEl = document.querySelector(".start");
+var welcome = document.querySelector(".welcome");
 var i = 0;
 var userChoice = "";
+var time;
+var count = 100;
+var finalScore = "";
 var testQuestions = [
   {
     prompt: "Commonly used data types DO Not include:",
@@ -35,65 +27,63 @@ var testQuestions = [
   },
   {
     prompt: "Arrays in JavaScript can be used to store ___.",
-    choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+    choices: [
+      "Numbers and Strings",
+      "Other Arrays",
+      "Booleans",
+      "All of the above",
+    ],
     answer: "All of the above",
   },
   {
-    prompt: "String values must be enclosed with ___ when being assigned to variables.",
+    prompt:
+      "String values must be enclosed with ___ when being assigned to variables.",
     choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
     answer: "Quotes",
   },
   {
-    prompt: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    prompt:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "Terminal/Bash", "For Loops", "console.log"],
     answer: "console.log",
   },
 ];
 
-answerOptions.addEventListener("click", function(event) {
-console.log(event.target);
-if (testQuestions[i].answer===event.target.textContent) {
-    
-}
-i++;
-displayQuestion();
+answerOptions.addEventListener("click", function (event) {
+  console.log(event.target);
+  if (testQuestions[i].answer === event.target.textContent) {
+  }
+  i++;
+  displayQuestion();
 });
 
-
-
-function displayQuestion (){
-quizQuestions.textContent = testQuestions[i].prompt
-choiceA.textContent = testQuestions[i].choices[0]
-choiceB.textContent = testQuestions[i].choices[1]
-choiceC.textContent = testQuestions[i].choices[2]
-choiceD.textContent = testQuestions[i].choices[3]
+function displayQuestion() {
+  quizQuestions.textContent = testQuestions[i].prompt;
+  choiceA.textContent = testQuestions[i].choices[0];
+  choiceB.textContent = testQuestions[i].choices[1];
+  choiceC.textContent = testQuestions[i].choices[2];
+  choiceD.textContent = testQuestions[i].choices[3];
 }
 
-//Timer that counts down from 100 seconds
-function countdown() {
-    var timeLeft = 100;
+
+
+function timerCountdown() {
+  time = setInterval(function () {
+    count--;
+    time.textContent = count;
+    if (count < 0) {
+      finish();
+    }
+  }, 1000);
 }
 
-var timeInterval = setInterval(function(){
-if (timeLeft > 1) {
-    timeEl.textContent = timeLeft;
-    timeLeft--;
-} else if (timeLeft===1) {
-    timerEl.textContent = timeLeft;
-    timeLeft--;
-} else {
-    timeEl.textContent= '';
-    clearInterval(timeInterval);
+function startQuiz() {
+  welcome.setAttribute("style", "display:none");
+  timerCountdown();
+  displayQuestion();
 }
-}, 1000);
+
+
+countdown();
 
 displayQuestion();
-
-
-
-
-// //var questionOne = {
-//     prompt: "Commonly used data types DO Not include:",
-//     choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-//     answer: "Alerts"
-// }
